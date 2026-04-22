21edb75a0c631c8ae5adf3edb2d1bac8d031fc68
asklootz <askl@uia.no>
2026-04-22 13:43:17 +0200

    Unit -test oppgave

---
+++ Eksamenseksempel_SvarForslag.Tests/UnitTest.cs
@@ -0,0 +1,14 @@
+﻿namespace Eksamenseksempel_SvarForslag.Tests;
+
+public class UnitTest
+{
+
+    // Del 1, oppgave 17
+    [Fact]
+    public void Add_TwoNumbers_ReturnsCorrectSum()
+    {
+        Calculator c = new Calculator();
+        int result = c.Add(2, 3);
+        Assert.Equal(5, result);
+    }
+}
